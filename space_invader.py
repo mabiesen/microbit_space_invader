b1930bd4b03ec34639a0c4cf11cb0674a7ea1f8c
Matthew Biesen <upandcomming88@gmail.com>
2017-05-06 20:20:16 -0500

    Update space_invader.py

--- space_invader.py
+++ space_invader.py
@@ -1,13 +1,28 @@
 
+# coords given as x
+player_coord = 0
 
-#class for the player
+# coords given as x y
+enemy_coords = [0,2]
 
-class player():
-
-  def _init_(self):
+# when the player fires a missile
+def missile():
+  ctr = 0
+  while ctr < 5:
+    set_pixel(player_coord,x,9)
+    ctr = ctr + 1
     
-
-
-#class for the enemy dots
+def move_enemy():
+  global enemy_coords
+  if enemy_coords[1] == 3:
+    if player_coord == enemy_coords[0]:
+      game_over()
+  elif enemy_coords[1] == 4:
+    game_over()
+      
+def game_over():
+  display.scroll("game over")
+  
+def start_game():
 
 
